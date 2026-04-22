ff4bc39d15469502794cf01076d93fa27c62b61b
cnb.c8l1G1dehKA <OlW6gWrcWPMd3ya1yu6oCB+cnb.c8l1G1dehKA@noreply.cnb.cool>
2026-04-22 19:30:15 +0800

    今天主要完成编辑页面中的图文编辑的UI设计以及依赖添加

---
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/screens/ImagePickerScreen.kt
@@ -0,0 +1,15 @@
+package org.example.project.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ImageSelectScreen() {
+    
+}
+
+
+@Composable
+@Preview
+fun ImageSelectScreenPreview() {
+}
