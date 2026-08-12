093157748bce195d989c0eb710e066b994ebfbf8
何聚敛 <2949282402@qq.com>
2026-08-12 09:18:27 +0800

    feat(storage): 建立StorageArea逻辑存储区域与StoragePath相对路径值类

---
+++ shared/src/commonMain/kotlin/org/example/project/core/storage/api/StoragePath.kt
@@ -0,0 +1,22 @@
+﻿/**
+ * @File: StoragePath.kt
+ * @Package: org.example.project.core.storage.api
+ * @Description: 逻辑存储相对路径封装值类，禁止传递绝对路径
+ * @Author: 何聚敛
+ * @Date: 2026-08-12
+ */
+package org.example.project.core.storage.api
+
+import kotlin.jvm.JvmInline
+
+/**
+ * 逻辑存储相对路径值类。
+ * 
+ * @property value 内部相对路径字符串 (如 "feedline/images/avatar.jpg")
+ */
+@JvmInline
+value class StoragePath(
+    val value: String
+) {
+    override fun toString(): String = value
+}
