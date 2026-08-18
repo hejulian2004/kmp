a9b312aed769809edbdd4e99226ee603a8931e96
何聚敛 <2949282402@qq.com>
2026-08-18 09:59:42 +0800

    test(storage): 补充FakeFileStorage内存映射与存储容器初始化单元测试

---
+++ shared/src/commonTest/kotlin/org/example/project/core/storage/client/AppStorageInitializerTest.kt
@@ -0,0 +1,25 @@
+﻿/**
+ * @File: AppStorageInitializerTest.kt
+ * @Package: org.example.project.core.storage.client
+ * @Description: AppStorageInitializer 单例初始化与容器获取单元测试套件
+ * @Author: 何聚敛
+ * @Date: 2026-08-18
+ */
+package org.example.project.core.storage.client
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class AppStorageInitializerTest {
+
+    @Test
+    fun testStorageInitializerInitAndContainer() {
+        AppStorageInitializer.init(null)
+
+        assertTrue(AppStorageInitializer.isInitialized, "存储架构应处于已初始化状态")
+        val container = AppStorageInitializer.container
+        assertNotNull(container, "StorageContainer 实例不应为空")
+        assertNotNull(container.fileStorage, "fileStorage 实例不应为空")
+    }
+}
